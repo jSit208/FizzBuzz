2353358c6a18ac1850769d28360cdf257b830e8f
jSit208 <75465885+jSit208@users.noreply.github.com>
2021-08-09 23:11:02 -0700

    Javascript code Completed
    Algorithm for Javascript completed. Still missing FizzBuzz Logo and The Code Page. App.html has been completed and working.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,56 @@
-//Call Hello World
-function HelloWorld() {
-    alert("Hello World");
+/*
+    Steps-
+        Retrieve Input Values (InputValues1, InputValues2), Validation to ensure Numeric
+        Generate List of values from 1 to 100
+        Display Results to Screen
+*/
+
+function getInputValues() {
+    document.getElementById("divResults").classList.add("invisible");
+
+    let input1 = document.getElementById("inputValue1").value;
+    let input2 = document.getElementById("inputValue2").value;
+    let numbers = [];
+
+    input1 = Number.parseInt(input1); input2 = Number.parseInt(input2);
+    if(Number.isInteger(input1) && Number.isInteger(input2)){
+        numbers = generateNumbers();
+        displayResults(numbers, input1, input2);
+    } else {
+
+    }
+}
+
+function generateNumbers() {
+    let numbersArray = [];
+    for(let i = 1; i <= 100; i++){
+        numbersArray.push(i);
+    }
+    return numbersArray;
+}
+function displayResults(numbersArray, inputValue1, inputValue2) {
+    let strTableResults = `<tr>`;
+
+    document.getElementById("divResults").classList.remove("invisible");
+
+    numbersArray.forEach(number => {
+        if(number % (inputValue1 * inputValue2) === 0) {
+            strTableResults += addDataItem("FizzBuzz", "fizzbuzz");
+        } else if (number % inputValue1 === 0) {
+            strTableResults += addDataItem("Fizz", "fizz");
+        } else if (number % inputValue2 === 0) {
+            strTableResults += addDataItem("Buzz", "buzz");
+        } else {
+            strTableResults += addDataItem(number, "none");
+        }
+
+        if(number % 5 === 0 && number !== 100){
+            strTableResults += `</tr><tr>`
+        }
+    });
+    strTableResults += `</tr>`;
+    document.getElementById("tblResults").innerHTML = strTableResults;
+}
+function addDataItem(value, classType){
+    return `<td class="${classType}">${value}</td>`;
 }
